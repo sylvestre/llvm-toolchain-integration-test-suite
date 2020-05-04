ed3b3b85d5ec74dc1e49288a06b7d1b5c1a8b262
Sylvestre Ledru <sylvestre@debian.org>
2020-05-04 11:00:14 +0200

    add a real example

--- tests/libclang_cpp.cpp
+++ tests/libclang_cpp.cpp
@@ -5,7 +5,20 @@
 // RUN: ldd %t 2>&1|grep -q libclang-cpp
 // RUN: %t
 
-#include <chrono>
-int main() {
-    return 0;
+#include "clang/Tooling/CommonOptionsParser.h"
+#include "llvm/Support/CommandLine.h"
+
+using namespace clang::tooling;
+
+// Apply a custom category to all command-line options so that they are the
+// only ones displayed.
+static llvm::cl::OptionCategory MyToolCategory("my-tool options");
+
+int main(int argc, const char **argv) {
+  // CommonOptionsParser constructor will parse arguments and create a
+  // CompilationDatabase.  In case of error it will terminate the program.
+  CommonOptionsParser OptionsParser(argc, argv, MyToolCategory);
+
+  // Use OptionsParser.getCompilations() and OptionsParser.getSourcePathList()
+  // to retrieve CompilationDatabase and the list of input file paths.
 }
